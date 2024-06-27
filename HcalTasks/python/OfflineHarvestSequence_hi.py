174f7f47764b90c026e470e240630ae052671b8c
salimcerci <salim.cerci@cern.ch>
2024-06-27 15:28:02 +0200

    Delete HcalTasks directory

--- HcalTasks/python/OfflineHarvestSequence_hi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from DQM.HcalTasks.HcalOfflineHarvesting_cfi import *
-
-# apply some customization
-# -	ptype = 1 Offlien processing
-# - runkey value 2 - cosmics
-hcalOfflineHarvesting.ptype = 1
-hcalOfflineHarvesting.runkeyVal = 4
-hcalOfflineHarvesting.runkeyName = "hi_run"
